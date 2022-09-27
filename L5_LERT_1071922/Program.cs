8e9c733262bc6ca72297a718e487966f0356e9be
luise <luise@DESKTOP-678KKUR>
2022-09-27 13:39:08 -0600

    Agregar archivos de proyecto.

---
+++ L5_LERT_1071922/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L5_LERT_1071922
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero;
+
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("Ingrese un numero entero");
+
+            numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroEntero > 0)
+            {
+                Console.WriteLine("Resultado : positivo");
+            }
+            else if (numeroEntero < 0)
+            {
+                Console.WriteLine("Resultado : negativo");
+            }
+            else if (numeroEntero == 0)
+            {
+                Console.WriteLine("Resultado : el valor ingresado es cero");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
